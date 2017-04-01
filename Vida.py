bbef767441e14c8ccf905fe8232a7109cd2cf6bb
DylanNS <dylan-ns@hotmail.com>
2017-04-01 19:43:59 -0300

    Classe Pontos de Vida

---
+++ Vida.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 19:14:38 2017
+
+@author: Dylan N. Sugimoto
+"""
+
+class Vida():
+    """
+    Representa os pontos de vida
+    """
+    def __init__(self, Max):
+        """
+        Max: Quantidade maxima de pontos de vida
+        """
+        self._MaxVida = Max
+        self.Vida = Max
+    def getLife(self):
+        return self.Vida
+    def Subtrair(self, Dano):
+        """
+        Dano: Quantidade de dano sofrido
+        """
+        self.Vida -=Dano
+        if self.Vida <0:
+            self.Vida = 0
+            
