56134de49dfd8713fdc2fbb9cb495de37c4a8f82
DylanNS <dylan-ns@hotmail.com>
2017-04-03 20:21:36 -0300

    Delete do atributo imagem

--- Arma.py
+++ Arma.py
@@ -8,26 +8,22 @@ Created on Sat Apr  1 18:48:42 2017
 class Arma():
     """Representa a ideia abstrata de arma"""
     
-    def __init__(self,Som, Projetil, Imagem, Dono = None):
+    def __init__(self,Som, Projetil, Dono = None):
         """
         Som:        referencia o som da arma
-        Imagem:     referencia a imagem da arma
         Projetil:   nome do projetil que a arma usa
         Dono:       nome da instancia que possui essa arma
         """
         self._Som = Som
         self._Projetil = Projetil
-        self._Imagem = Imagem
         self.Dono = Dono
         
     def getSom(self):
         return self._Som
-    def getImagem(self):
-        return self._Imagem
     def getProjetil(self):
         return self._Projetil
     def getDono(self):
         return self._Dono
-    def comprou(self,Dono):
+    def setDono(self,Dono):
         self.Dono = Dono
         
