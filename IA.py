d10b8ebecb2da3486bcda34bf5ef127803a2b4d7
DylanNS <dylan-ns@hotmail.com>
2017-04-11 17:24:06 -0300

    comecando aviaoinimigo

--- IA.py
+++ IA.py
@@ -8,11 +8,11 @@ import motor
 import math
 
 velPadrao = 10 #constante de valor padrao de velocidade para IA
-distanciaManobra = 300
-distanciaPerseguir = 200
-aceleracaoAngular = 1
-aceleracao = 1
-desaceleracao = 1
+distanciaManobra = 300 #valor constante para distancia minima para realizar Manobra180V
+distanciaPerseguir = 200#distancia maxima no qual IA comeca diminuir velocidade
+aceleracaoAngular = 1#velocidade com que IA rotaciona
+aceleracao = 1#rapidez com que IA aumenta a sua velocidade em X
+desaceleracao = 1#rapidez com que IA diminui a sua velocidade em X
 
 class IA():
     def __init__(self,arma, pos = motor.Ponto(0,0), vel=motor.Ponto(velPadrao,0), alvoPos = motor.Ponto(0,0),
@@ -46,6 +46,7 @@ class IA():
         self.alvoPos = alvoPos
         self.alvoVel = alvoVel
         self.angUni = angUni
+        
         #escuta o evento e chama a funcao
         #PlayerLocation: é o evento da posicao do player
         self.even.escutar("PlayerLocation", self.localizar)
@@ -64,9 +65,67 @@ class IA():
         difX = self.Pos.getX() - alvo[0]
         self.angUni.set(math.atan2(difY,difX))
         
+    def aim(self):
+        """
+        Diminui o angulo de visada para zero.
+        E se o angulo de visada estiver dentro da tolerancia de disparo
+        ,então, atira.
+        """
+        AngVisada = self.ang.getAngulo() - self.angUni.getAngulo()
+        if AngVisada >0:
+            self.velAng = -aceleracaoAngular
+        elif AngVisada < 0 :
+            self.velAng = +aceleracaoAngular
+        elif AngVisada == 0:
+            self.velAng = 0
+        if AngVisada <= self.deltaAngTol:
+            self.shoot()
+ 
+    def shoot(self):
+        """
+        lanca evento de disparo.
+        tupla_tiro: (PosicaodaIA, direcaoDeDIsparo,ObjetoArma)
+        """
+        tupla_tiro = (self.Pos.clonar(),self.ang.getAngulo(),
+                     self.arma)
+        self.even.lancar("Atirar",tupla_tiro)
+
+class AviaoInimigo(IA,motor.Figura):
+    
+    
+    def __init__(self,img,audio, arma, pos = motor.Ponto(0,0), vel=motor.Ponto(velPadrao,0), alvoPos = motor.Ponto(0,0),
+                 alvoVel = motor.Ponto(0,0), ang = motor.Angulo(0), angUni = motor.Angulo(0), 
+                 deltaAngTol = motor.Angulo(5)): 
+        IA.__init__(self,arma, pos = motor.Ponto(0,0), vel=motor.Ponto(velPadrao,0), alvoPos = motor.Ponto(0,0),
+                 alvoVel = motor.Ponto(0,0), ang = motor.Angulo(0), angUni = motor.Angulo(0), 
+                 deltaAngTol = motor.Angulo(5))
+        motor.Figura.__init__(img)
+        """
+        img:     É a string do nome do arquivo imagem do aviao
+        audio:   É a string do nome do arquivo audio do aviao
+        alvoPos: (posXdoJogador,posYdoJogador)
+        alvoVel: (velXdoJogador,velYdoJogador)
+        arma:    Objeto do tipo Arma
+        pos:     tupla de posicao do IA
+        vel:     tupla de velocidade do IA
+        ang:     inclinacao angular da reta definida 
+                 pelo vetor velocidade de IA
+        angUni:  inclinacao angular da reta que uni
+                 o alvo e a IA
+        velAng:  velocidade angular de IA
+        distanciaPerseguir: distancia minima para o qual o IA 
+                            tenta estabelecer a mesma velocidade que a do Jogador
+        deltaAngTol: angulo de tolerancia para disparo
+        """
         
+        self._audio = audio
+        self.even.lancar("tocar_efeito",self._audio)
+
     def perseguir(self):
-        
+        """
+        Ajusta a velocidade X da IA, de acordo com a posicao relativa 
+        do Jogador com a IA
+        """
         if self.Pos.getX() < self.alvoPos.getX() and self.Vel.getX() < 0:
                """
                Inverte a direcao de voo
@@ -107,29 +166,5 @@ class IA():
                     self.Vel.setX(acelX)
                 elif self.alvoVel.getX() != 0:
                     self.Vel.setX(self.alvoVel.getX())
-                    
-    def aim(self):
-        """
-        Diminui o angulo de visada para zero.
-        E se o angulo de visada estiver dentro da tolerancia de disparo
-        ,então, atira.
-        """
-        AngVisada = self.ang.getAngulo() - self.angUni.getAngulo()
-        if AngVisada >0:
-            self.velAng = -aceleracaoAngular
-        elif AngVisada < 0 :
-            self.velAng = +aceleracaoAngular
-        elif AngVisada == 0:
-            self.velAng = 0
-        if AngVisada <= self.deltaAngTol:
-            self.shoot()
- 
-    def shoot(self):
-        """
-        lanca evento de disparo.
-        tupla_tiro: (PosicaodaIA, direcaoDeDIsparo,ObjetoArma)
-        """
-        tupla_tiro = (self.Pos.clonar(),self.ang.getAngulo(),
-                     self.arma)
-        self.even.lancar("Atirar",tupla_tiro)
-    
+    def voar(self):
+        pass          
