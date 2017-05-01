138a76ec975f75db49e59c929a0328a1b2ee5e8a
DylanNS <dylan-ns@hotmail.com>
2017-05-01 19:31:58 -0300

    Classe Projetil: Implementado metodo atualiza
    metodo atualiza
    Atualiza a posicao do projetil com base na velocidade inicializada

--- Projetil.py
+++ Projetil.py
@@ -5,6 +5,7 @@ Created on Sat Apr  1 18:57:11 2017
 @author: Dylan N. Sugimoto
 """
 import motor
+import math
 
 class Projetil(motor.Figura):
     """
@@ -16,6 +17,7 @@ class Projetil(motor.Figura):
         Som:    referencia o som do impacto do projetil
         Dano:   Quantidade de dano causado
         Dono:   Quem atirou
+        veli:   modulo da velocidade do projetil
         """
         self._string_imagem = Imagem
         self._Som = Som
@@ -34,17 +36,35 @@ class Projetil(motor.Figura):
         return self.Dono
     def definirDono(self,Dono):
         self.Dono = Dono
-    def fisicaDeVoo(self,posI,velI):
+    def atualiza(self,dt):
         """
         Algoritmo da fisica de voo. Calcula posicao final
-        ,velocidade final e aceleracao final.
-        """
-        pass
+        """ 
+        #Calula a velocidade X e Y
+        velX = math.cos(self.direcao.getAngulo(False))*self.veli
+        velY = math.sin(self.direcao.getAngulo(False))*self.veli
+        #Truncamento
+        VelocidadeX = int(velX)
+        VelocidadeY = int(velY)
+        #Arredondamento
+        if velX - VelocidadeX >=0.5:
+            VelocidadeX = VelocidadeX+1
+        elif VelocidadeX - velX >= 0.5:
+            VelocidadeX = VelocidadeX-1
+        if velY - VelocidadeY >= 0.5:
+            VelocidadeY = VelocidadeY+1
+        elif VelocidadeY - velY >= 0.5:
+            VelocidadeY = VelocidadeY-1
+        #calcula nova posicao
+        posX = self.Pos.getX() + VelocidadeX
+        posY = self.Pos.getY() + VelocidadeY
+        #atualiza posicao
+        self.Pos.setXY( posX, posY)
     def fisicaDeImpacto(self, posI,velI,acelI):
         """
         Algoritmo da fisica de impacto. 
         """
         self.even.lancar("tocarEfeito",self._Som)
     def Disparo(self, posI,direcao):
-        self.Pos = posI
-        self.direcao = direcao
+        self.Pos.setXY( posI.getX, posI.getY)
+        self.direcao = motor.Angulo(direcao)
