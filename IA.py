4bea4b2ee90fe9982c21dd85b0fd382604f8b455
DylanNS <dylan-ns@hotmail.com>
2017-05-05 21:42:05 -0300

    apagado codigos inutilizado

--- IA.py
+++ IA.py
@@ -85,60 +85,7 @@ class IA(motor.Renderizavel, Vida):
         self.alvoPos.setXY(alvo[0],alvo[1])
         self.alvoVel.setXY(alvo[2],alvo[3])
         
-        difY = self.Pos.getY() - alvo[1]
-        difX = self.Pos.getX() - alvo[0]
-        self.angUni.setAngulo(math.atan2(difY,difX),False)
-        
-    def aim(self):
-        """
-        Diminui o angulo de visada para zero.
-        E se o angulo de visada estiver dentro da tolerancia de disparo
-        ,então, atira.
-        """
-        AngVisada = self.ang.getAngulo() - self.angUni.getAngulo()
-           
-        if self.Vel.getX() > 0 and self.Pos.getX() >= self.alvoPos.getX():
-            #Se o jogador estiver nas costas da IA
-            if self.ang.getAngulo() == 0:
-                #e IA estiver na horizontal,
-                #IA mantem a direcao da velocidade
-                self.velAng = 0
-            elif self.ang.getAngulo() >0:
-                #e IA estiver subindo
-                #IA tenta voltar para voo horizontal
-                self.velAng = -aceleracaoAngular
-            elif self.ang.getAngulo() < 0:
-                #e IA estiver descendo
-                #IA tenta voltar para voo horizontal
-                self.velAng = +aceleracaoAngular
-                
-        elif self.Vel.getX() < 0 and self.Pos.getX() <= self.alvoPos.getX():
-            #Se o jogador estiver nas costas da IA
-            if self.ang.getAngulo() == 0:
-                #e IA estiver na horizontal,
-                #IA mantem a direcao da velocidade
-                self.velAng = 0
-            elif self.ang.getAngulo() >0:
-                #e IA estiver subindo
-                #IA tenta voltar para voo horizontal
-                self.velAng = -aceleracaoAngular
-            elif self.ang.getAngulo() < 0:
-                #e IA estiver descendo
-                #IA tenta voltar para voo horizontal
-                self.velAng = +aceleracaoAngular
-        else:
-            #Ajustando a mira
-            if AngVisada >0 and self.alvoPos.getX() - self.Pos.getX()> self.distanciaMira:
-                self.velAng = -aceleracaoAngular
-            elif AngVisada < 0 and self.Pos.getX() - self.alvoPos.getX()>self.distanciaMira:
-                self.velAng = +aceleracaoAngular
-            elif AngVisada == 0:
-                self.velAng = 0
-            if AngVisada <= self.deltaAngTol.getAngulo():
-               #Atirar
-               self.shoot()
 
-        
     def shoot(self,dt):
         """
         lanca evento de disparo.
@@ -206,7 +153,7 @@ class AviaoInimigo(IA,motor.Figura):
         self.Manobra180V = False
         self._string_som_explosao = string_som_explosao
         self._audio = audio
-        self.even.lancar("tocarEfeito",self._audio)
+       # self.even.lancar("tocarEfeito",self._audio)
         
         self.distanciaReacao = 30
         
@@ -223,93 +170,7 @@ class AviaoInimigo(IA,motor.Figura):
             self.setString(self.img1)
             #Troca o estado da manobra
             #voando pra esquerda
-  
-    def perseguir(self):
-        """
-        Ajusta a velocidade X da IA, de acordo com a posicao relativa 
-        do Jogador com a IA
-        """
-        if (self.Pos.getX() < self.alvoPos.getX()) and (self.Vel.getX() < 0):
-               """
-               Inverte a direcao de voo
-               """
-               if self.alvoPos.getX() - self.Pos.getX() > self.distanciaManobra:
-                   self.Vel.setX(0)
-                   self.realizarManobra180V()
-                   self.Vel.setX(-velPadrao)
-               elif self.Vel.getX() >= self.alvoVel.getX():
-                   acelX = self.Vel.getX() - aceleracao
-                   self.Vel.setX(acelX)
-        elif (self.Pos.getX() > self.alvoPos.getX()) and (self.Vel.getX() > 0):
-              """
-              Inverte a direcao de voo
-              """
-              if self.Pos.getX() - self.alvoPos.getX() > self.distanciaManobra:
-                  self.Vel.setX(0)
-                  self.realizarManobra180V()
-                  self.Vel.setX(velPadrao)
-              elif self.Vel.getX() <= self.alvoVel.getX():
-                  acelX = self.Vel.getX() + aceleracao
-                  self.Vel.setX(acelX)   
-                   
-        elif self.Pos.distancia2(self.alvoPos) <self.distanciaPerseguir:
-            """
-            se tiver a uma certa distancia, diminui a sua velocidade 
-            para igualar à velocidade do jogador.
-            """
-            if self.Vel.getX() > self.alvoVel.getX() and self.Vel.getX()>0 and\
-               self.Pos.getX()< self.alvoPos.getX():
-                   
-                acelX = self.Vel.getX() - desaceleracao
-                if acelX >= self.alvoVel.getX() and acelX > 0:
-                    self.Vel.setX(acelX)
-                elif self.alvoVel.getX() > 0 and self.Vel.getX()<= self.alvoVel.getX():
-                    self.Vel.setX(self.alvoVel.getX())
-                
-            elif self.Vel.getX() < self.alvoVel.getX() and self.Vel.getX() < 0\
-                 and self.Pos.getX() > self.alvoPos.getX():
-                acelX = self.Vel.getX() + desaceleracao
-                if acelX <= self.alvoVel.getX() and acelX < 0:
-                    self.Vel.setX(acelX)
-                elif self.alvoVel.getX() < 0 and\
-                    self.Vel.getX() > self.alvoVel.getX():
-                        
-                    self.Vel.setX(self.alvoVel.getX())
-            
-    def voar(self,dt):
-        
-       #atualiza a posicao para o frame seguinte
-       acrescimoX = int(self.Vel.getX()*dt)
-       acrescimoY = int(self.Vel.getY()*dt)
-       self.Pos.soma(motor.Ponto(acrescimoX,acrescimoY)) 
-       #atualiza a posicao da Figura
-       self.pos.setXY(self.Pos.getX(),self.Pos.getY())       
-       if self.velAng != 0: 
-           """
-           Se a velocidade angular nao for zero, tem que rotacionar a velocidade
-           e a figura de um angulo igual ao modulo da velocidade angular.
-           Logo, o novo Vx e novo Vy sao as projecoes do modulo de V.
-           """
-           ang = math.atan2(self.Vel.getY(),self.Vel.getX()) + self.velAng
-           self.ang.setAngulo(ang,False) #novo angulo com a horizontal
-           self.rot.setAngulo(self.ang.getAngulo())#atualiza o angulo da Figura
-           projX = self.Vel.distancia(motor.Ponto(0,0))*math.cos(self.ang.getAngulo(False))
-           projY = self.Vel.distancia(motor.Ponto(0,0))*math.sin(self.ang.getAngulo(False))
-           NovoVx = int(projX)#deve ser inteiro para alterar a posicao
-           NovoVy = int(projY)#deve ser inteiro para posicao ser inteira (pixel)
-           if NovoVx == 0 and ((-math.pi/2)<self.ang.getAngulo()<(math.pi/2)):
-               #se a velocidade horizontal for zerada
-               #atribui um valor padrao
-               NovoVx = -velPadrao
-           elif NovoVx == 0 and ((math.pi/2)<self.ang.getAngulo() or\
-                                 self.ang.getAngulo() <(-math.pi/2)):
-               #se a velocidade horizontal for zerada
-               #atribui um valor padrao
-               NovoVx = velPadrao
-           if  NovoVy == 0 and projY!= 0:
-               #se o truncamento zerar a velocidade vertical nao nula
-               NovoVy = 1
-           self.Vel.setXY(NovoVx,NovoVy)
+
         
         
     def mira(self, dt):
@@ -326,7 +187,7 @@ class AviaoInimigo(IA,motor.Figura):
         else:
             vel = -self.velMax
         #if abs(dif) < self.deltaAngTol.getAngulo():
-            #self.shoot(dt)
+         #   self.shoot(dt)
             
         # Runge-Kutta de primeira ordem :D
         self.rot.incrementa(vel*dt) 
